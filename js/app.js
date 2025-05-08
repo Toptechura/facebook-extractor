7f6f8b42013f5858a5280014cba2cc8566d3df16
Toptechura <toptechura@gmail.com>
2025-05-08 10:57:15 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,11 +9,22 @@ window.fbAsyncInit = function () {
     xfbml: true,
     version: "v18.0", // Use a versão mais recente disponível
   });
-
-  // Verificar status de login
-  FB.getLoginStatus(function (response) {
+  FB.AppEvents.logPageView();
+  
+  // Verificar status de login após inicialização
+  FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
+};
+
+// Carregamento assíncrono do SDK
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/pt_BR/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 };
 
 // Callback quando o status de login muda
